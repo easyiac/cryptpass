a70138bedcee490aaf3455e7da743c445f05edcc
arpan <arpan.rec@gmail.com>
2025-04-11 22:53:31 +0530

    feat: add buildkitd configuration and CA certificate for Docker registry

--- docker-push.sh
+++ docker-push.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 version=$(cat CRYPTPASS_VERSION)
 
-docker buildx create --name cryptpass-builder --driver docker-container --bootstrap || true
+docker buildx create --name cryptpass-builder --driver docker-container --bootstrap \
+    --buildkitd-config buildkitd.toml || true
 
 docker buildx build -f Dockerfile . \
     --builder cryptpass-builder \
@@ -11,6 +12,10 @@ docker buildx build -f Dockerfile . \
     -t "docker.io/arpanrecme/cryptpass:rust" \
     -t "docker.io/arpanrecme/cryptpass:rust-${version}" \
     -t "docker.io/arpanrecme/cryptpass:latest" \
+    -t "10.8.33.192:8008/cryptpass/cryptpass:${version}" \
+    -t "10.8.33.192:8008/cryptpass/cryptpass:rust" \
+    -t "10.8.33.192:8008/cryptpass/cryptpass:rust-${version}" \
+    -t "10.8.33.192:8008/cryptpass/cryptpass:latest" \
     --platform linux/amd64,linux/arm64 \
     --output type=registry \
     --network=none
