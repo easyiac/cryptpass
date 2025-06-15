a631aed2ea9a8decf106181f99fc1cfcd8501670
arpan <me@arpanrec.com>
2025-06-16 02:40:21 +0530

    fix: upgrade ansible-galaxy collection installation to ensure latest versions

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -9,7 +9,7 @@ Setup Cryptpass: \
 
 log_message "Installing required collections and roles"
 poetry install
-poetry run ansible-galaxy collection install community.general
-poetry run ansible-galaxy collection install community.docker
+poetry run ansible-galaxy collection install community.general --upgrade
+poetry run ansible-galaxy collection install community.docker --upgrade
 poetry run ansible-galaxy role install geerlingguy.docker
 poetry run ansible-playbook ansible/playbook.yaml
