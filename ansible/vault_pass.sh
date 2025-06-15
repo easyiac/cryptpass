6a1ac2a035fac47d3426dcfea93550a7b3b20a27
arpan <me@arpanrec.com>
2025-06-15 23:47:30 +0530

    fix: update item reference in vault_pass.sh to use ANSIBLE_VAULT_PASS

--- ansible/vault_pass.sh
+++ ansible/vault_pass.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 bw sync --quiet
-bw_item=$(bw get item cryptpass --raw)
+bw_item=$(bw get item ANSIBLE_VAULT_PASS --raw)
 
-echo "${bw_item}" | jq '.fields[] | select(.name == "ANSIBLE_VAULT_PASS").value' -c -r
+echo "${bw_item}" | jq '.fields[] | select(.name == "easyiac_cryptpass").value' -c -r
